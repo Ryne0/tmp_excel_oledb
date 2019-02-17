2803271ee68ada6e77e309301b76973575f5d685
Ryne <ryneryder@gmail.com>
2019-02-17 23:38:41 +0800

    excel oledb sample code

---
+++ SampleCode/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Excel_using_OleDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.cmdReadExcel = new System.Windows.Forms.Button();
+            this.cmdBrowse = new System.Windows.Forms.Button();
+            this.lblBrowse = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmdWriteInExcel = new System.Windows.Forms.Button();
+            this.cmdUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdCancel.Location = new System.Drawing.Point(535, 63);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(95, 25);
+            this.cmdCancel.TabIndex = 0;
+            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            this.cmdCancel.Click += new System.EventHandler(this.cmdCancel_Click);
+            // 
+            // cmdReadExcel
+            // 
+            this.cmdReadExcel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdReadExcel.Location = new System.Drawing.Point(249, 63);
+            this.cmdReadExcel.Name = "cmdReadExcel";
+            this.cmdReadExcel.Size = new System.Drawing.Size(137, 25);
+            this.cmdReadExcel.TabIndex = 1;
+            this.cmdReadExcel.Text = "Read Excel Content";
+            this.cmdReadExcel.UseVisualStyleBackColor = true;
+            this.cmdReadExcel.Click += new System.EventHandler(this.cmdReadExcel_Click);
+            // 
+            // cmdBrowse
+            // 
+            this.cmdBrowse.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdBrowse.Location = new System.Drawing.Point(710, 35);
+            this.cmdBrowse.Name = "cmdBrowse";
+            this.cmdBrowse.Size = new System.Drawing.Size(95, 25);
+            this.cmdBrowse.TabIndex = 2;
+            this.cmdBrowse.Text = "Browse";
+            this.cmdBrowse.UseVisualStyleBackColor = true;
+            this.cmdBrowse.Click += new System.EventHandler(this.cmdBrowse_Click);
+            // 
+            // lblBrowse
+            // 
+            this.lblBrowse.AutoSize = true;
+            this.lblBrowse.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBrowse.Location = new System.Drawing.Point(12, 19);
+            this.lblBrowse.Name = "lblBrowse";
+            this.lblBrowse.Size = new System.Drawing.Size(158, 19);
+            this.lblBrowse.TabIndex = 3;
+            this.lblBrowse.Text = "Browse XLS or XSLX file";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 37);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(692, 20);
+            this.txtFilePath.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 110);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(773, 170);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // cmdWriteInExcel
+            // 
+            this.cmdWriteInExcel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdWriteInExcel.Location = new System.Drawing.Point(106, 63);
+            this.cmdWriteInExcel.Name = "cmdWriteInExcel";
+            this.cmdWriteInExcel.Size = new System.Drawing.Size(137, 25);
+            this.cmdWriteInExcel.TabIndex = 6;
+            this.cmdWriteInExcel.Text = "Write in Excel";
+            this.cmdWriteInExcel.UseVisualStyleBackColor = true;
+            this.cmdWriteInExcel.Click += new System.EventHandler(this.cmdWriteInExcel_Click);
+            // 
+            // cmdUpdate
+            // 
+            this.cmdUpdate.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdUpdate.Location = new System.Drawing.Point(392, 63);
+            this.cmdUpdate.Name = "cmdUpdate";
+            this.cmdUpdate.Size = new System.Drawing.Size(137, 25);
+            this.cmdUpdate.TabIndex = 7;
+            this.cmdUpdate.Text = "Update in Excel";
+            this.cmdUpdate.UseVisualStyleBackColor = true;
+            this.cmdUpdate.Click += new System.EventHandler(this.cmdUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 316);
+            this.Controls.Add(this.cmdUpdate);
+            this.Controls.Add(this.cmdWriteInExcel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lblBrowse);
+            this.Controls.Add(this.cmdBrowse);
+            this.Controls.Add(this.cmdReadExcel);
+            this.Controls.Add(this.cmdCancel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdCancel;
+        private System.Windows.Forms.Button cmdReadExcel;
+        private System.Windows.Forms.Button cmdBrowse;
+        private System.Windows.Forms.Label lblBrowse;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button cmdWriteInExcel;
+        private System.Windows.Forms.Button cmdUpdate;
+    }
+}
+
---
+++ SampleCode/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Excel_using_OleDB
+{
+    public partial class Form1 : Form
+    {
+
+        string connectionString = "";
+        OleDbCommand cmd = null;
+        OleDbConnection objCon = null;
+        OleDbDataAdapter objDA = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdBrowse_Click(object sender, EventArgs e)
+        {
+            // Show the dialog and get result.
+            openFileDialog1.Filter = "Excel files|*.xls;*.xlsx";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                txtFilePath.Text = openFileDialog1.SafeFileName;
+            }
+            Console.WriteLine(result);
+        }
+
+        private void cmdCancel_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void cmdReadExcel_Click(object sender, EventArgs e)
+        {
+
+            DataSet objDS = new DataSet();
+
+            string szConStr = collect_Con(System.IO.Path.GetExtension(txtFilePath.Text), txtFilePath.Text);
+
+
+            try
+            {
+                objCon = new OleDbConnection(szConStr);
+                objCon.Open();
+                objDA = new System.Data.OleDb.OleDbDataAdapter("select * from [Sheet1$]", objCon);
+                DataTable objDTExcel = new DataTable();
+                objDA.Fill(objDTExcel);
+                dataGridView1.DataSource = objDTExcel;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (objDA != null)
+                {
+                    objDA.Dispose();
+                    objDA = null;
+                }
+
+                if (objCon != null)
+                {
+                    objCon.Close();
+                    objCon.Dispose();
+                    objCon = null;
+                }
+            }
+
+
+        }
+
+
+
+        private string collect_Con(string szFileExtension, string szPath)
+        {
+
+            switch (szFileExtension.ToUpper())
+            {
+                case ".XLS":
+                    connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=
+                                        " + szPath + ";Extended Properties=\"Excel 8.0;HDR=YES;\"";
+                    break;
+                case ".XLSX":
+                    connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=
+                                        " + szPath + ";Extended Properties=\"Excel 12.0;HDR=YES;\"";
+                    break;
+                default:
+                    break;
+            }
+
+            return connectionString;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtFilePath.Text = Application.StartupPath + "\\TestBook.xlsx";
+            connectionString = collect_Con(System.IO.Path.GetExtension(txtFilePath.Text), txtFilePath.Text);
+        }
+
+        private void cmdWriteInExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                objCon = new OleDbConnection(connectionString);
+                objCon.Open();
+                cmd = new OleDbCommand();
+                cmd.Connection = objCon;
+
+                cmd.CommandText = "Insert into [Sheet1$] (month,mango,apple,orange) VALUES('DEC','40','60','80');";
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+                //exception here
+            }
+            finally
+            {
+                objCon.Close();
+                objCon.Dispose();
+            }
+        }
+
+        private void cmdUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                objCon = new OleDbConnection(connectionString);
+                objCon.Open();
+                cmd = new OleDbCommand();
+                cmd.Connection = objCon;
+                cmd.CommandText = "UPDATE [Sheet1$] SET month = 'DEC' WHERE apple = 60;";
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+                //exception here
+            }
+            finally
+            {
+                objCon.Close();
+                objCon.Dispose();
+            }
+
+        }
+    }
+}
